bc743007fc5f7554d9fe3367255b7c9116ab4d13
Carda01 <1000021334@studium.unict.it>
2025-05-18 20:08:44 +0200

    for all category news put to trusted

--- dags/news_trusted_script.py
+++ dags/news_trusted_script.py
@@ -10,27 +10,32 @@ spark, base_path = get_spark_and_path(is_gcs_enabled)
 landing_path = get_landing_path(base_path)
 trusted_path = get_trusted_path(base_path)
 
-table_subpath = 'delta_news/sports'
-loader = IncrementalLoader(spark, landing_path, table_subpath)
-df = loader.get_new_data()
 
-processor = NewsProcessor(spark, df)
 
-logging.info("Begin processing")
-logging.info(processor.df.head(3))
-processor.ensure_schema()
-processor.remove_clear_duplicates()
-processor.name_to_id()
-processor.remove_hidden_duplicates(['url'], ['publishedAt'])
-processor.normalize_text(['title', 'description', 'content'])
-processor.expand_source()
-processor.order_by('publishedAt', ascending=False)
+CATEGORIES = ['entertainment', 'sports', 'technology']
+for category in CATEGORIES:
+    logging.info(f"Processing category {category}")
+    table_subpath = f'delta_news/{category}'
+    loader = IncrementalLoader(spark, landing_path, table_subpath)
+    df = loader.get_new_data()
 
-logging.info("End processing")
-logging.info(processor.df.head(3))
+    processor = NewsProcessor(spark, df)
 
-save_path = os.path.join(trusted_path, table_subpath)
-processor.merge_with_trusted(save_path, ['url'])
+    logging.info("Begin processing")
+    logging.info(processor.df.head(3))
+    processor.ensure_schema()
+    processor.remove_clear_duplicates()
+    processor.name_to_id()
+    processor.remove_hidden_duplicates(['url'], ['publishedAt'])
+    processor.normalize_text(['title', 'description', 'content'])
+    processor.expand_source()
+    processor.order_by('publishedAt', ascending=False)
 
-logging.info("Data was merged")
+    logging.info("End processing")
+    logging.info(processor.df.head(3))
+
+    save_path = os.path.join(trusted_path, table_subpath)
+    processor.merge_with_trusted(save_path, ['url'])
+
+    logging.info("Data was merged")
 
